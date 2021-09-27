741f2789037b0773ac105f50ea07d89c41025e74
dongxin <x.dong@knowyourself.cc>
2021-09-27 17:33:12 +0800

    处理预请求

--- server.js
+++ server.js
@@ -32,9 +32,13 @@ app.all('*', (req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Headers", "X-Requested-With");
     res.setHeader("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-    res.setHeader("X-Powered-By", ' 3.2.1')
+    res.setHeader("X-Powered-By", '3.2.1')
     res.setHeader("Content-Type", "application/json;charset=utf-8");
-    next();
+    if (req.method.toLowerCase() === 'options') {
+        res.send(200)
+    } else {
+        next();
+    }
 })
 
 app.use('/api', TopicRouter)
